6c40afe5123e679b66cde9c7ec98f832498f7237
Santhuvaddelli <santhuvaddelli13@gmail.com>
2026-02-14 20:38:21 +0530

    port commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://51.21.190.186',
+    baseURL: 'http://51.21.190.186:80',
 });
 
 //Attach AccessToken to every request after login
@@ -20,7 +20,7 @@ api.interceptors.response.use((response) => response, async (error) => {
         originalRequest._retry = true;
         try {
             const refreshToken = localStorage.getItem("refreshToken");
-            const res = await api.post("http://51.21.190.186/api/refresh", { refreshToken });
+            const res = await api.post(`${api.baseURL}/api/refresh`, { refreshToken });
             const newAccessToken = res.data.accessToken;
             localStorage.setItem("accessToken", newAccessToken);
             originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
